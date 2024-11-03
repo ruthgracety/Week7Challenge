84cffb90ca9477e5b67ac6146c25ba32b1093e2d
ruthgracety <ruthgracety@gmail.com>
2024-11-03 23:58:23 +0000

    added API key

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "ccc4b1tc7of0e402eb0f24b35faed2d9";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
